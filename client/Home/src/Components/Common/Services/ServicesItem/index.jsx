5c6c06293fe883c638a6725e817b3840cee48c9a
mohannadhanafi <mohannad.hanafi96@gmail.com>
2019-04-23 15:43:50 +0300

    s

--- client/Home/src/Components/Common/Services/ServicesItem/index.jsx
+++ client/Home/src/Components/Common/Services/ServicesItem/index.jsx
@@ -13,12 +13,12 @@ export default class index extends Component {
             <div className="row">
               {contacts.slice(0, 6).map(service => (
                 <div key={uuid()} className="col-lg-4">
-                  <Link to={`/service/${service.id}`} className="service box-shadow hover-line hover-down">
+                  <a className="service box-shadow hover-line hover-down">
                     {/* <i className={`service__icon ${service.icon}`} /> */}
                     <img className="service__icon" src={`/api/v2/getFile/${service.icon}`} alt="icon" />
                     <h4 className="service__title">{service.title}</h4>
                     <p className="service__text">{service.desc}</p>
-                  </Link>
+                  </a>
                 </div>
               ))}
             </div>
