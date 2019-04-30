064bc83d5080a9b8adc8551de56ed68397301f18
mohannadhanafi <mohannad.hanafi96@gmail.com>
2019-04-30 14:30:53 +0300

    s

--- client/Home/src/Components/Common/Disable/index.jsx
+++ client/Home/src/Components/Common/Disable/index.jsx
@@ -6,14 +6,14 @@ import './style.css';
 function index({ options }) {
   return (
     <div className="layout-minimal main--div" style={{ background: 'url(https://i.ibb.co/BPnN55B/image-13.jpg) no-repeat' }}>
-      <div className="site-container">
+      <div className="container">
 
         <header className="site-header">
-          {/* {options && (
+          {options && (
           <a href="/" className="logo mx-auto">
-            <img src={`/api/v2/getFile/${options[0].logo}`} alt="" />
+            <img className="logo" src={`/api/v2/getFile/${options[0].logo}`} alt="logo" />
           </a>
-          )} */}
+          )}
         </header>
 
         <div className="home-block">
@@ -22,21 +22,22 @@ function index({ options }) {
             <div id="home" className="d-flex min-vh--100 section">
               <div className="container align-self-center">
                 <h1 className="mb-3 h1">We bulid beautiful digital experiences.</h1>
-                <p className="lead mb-5 p">Our website is under construction. We`ll be here soon with our new awesome site, subscribe to be notified.</p>
+                <p className="lead mb-5 p">Our website is under construction. We`ll be here soon with our new awesome site</p>
                 <div className="row">
                   <div className="col-12 col-md-8 col-lg-7" />
                 </div>
               </div>
             </div>
+            {options && (
 
             <nav className="usefull-nav usefull-nav-pinned d-none d-xl-flex">
               <ul>
-                <li><a href="#"><i className="fab fa-facebook-f" /></a></li>
-                <li><a href="#"><i className="fab fa-twitter" /></a></li>
-                <li><a href="#"><i className="fab fa-instagram" /></a></li>
-                <li><a href="#"><i className="fab fa-pinterest" /></a></li>
+                <li><a href={options[0].facebook}><i className="ui-facebook" /></a></li>
+                <li><a href={options[0].twitter}><i className="ui-twitter" /></a></li>
+                <li><a href={options[0].instagram}><i className="ui-instagram" /></a></li>
               </ul>
             </nav>
+            )}
 
           </div>
         </div>
