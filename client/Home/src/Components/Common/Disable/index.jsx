bd82e556ebcdafce3fabb8a040bd4435c6f1801f
mohannadhanafi <mohannad.hanafi96@gmail.com>
2019-04-30 16:44:29 +0300

    w

--- client/Home/src/Components/Common/Disable/index.jsx
+++ client/Home/src/Components/Common/Disable/index.jsx
@@ -6,6 +6,13 @@ import './style.css';
 function index({ options }) {
   return (
     <div className="layout-minimal main--div" style={{ background: 'url(https://i.ibb.co/BPnN55B/image-13.jpg) no-repeat' }}>
+      <div className="global-overlay shadow-9">
+        <div className="overlay-inner bg-white opacity-70" />
+        <div className="frame">
+          <div className="frame-inner frame-inner-a bg-primary" />
+          <div className="frame-inner frame-inner-b bg-primary" />
+        </div>
+      </div>
       <div className="container">
 
         <header className="site-header">
@@ -32,9 +39,9 @@ function index({ options }) {
 
             <nav className="usefull-nav usefull-nav-pinned d-none d-xl-flex">
               <ul>
-                <li><a href={options[0].facebook}><i className="ui-facebook" /></a></li>
-                <li><a href={options[0].twitter}><i className="ui-twitter" /></a></li>
-                <li><a href={options[0].instagram}><i className="ui-instagram" /></a></li>
+                <li><a className="icon--link" href={options[0].facebook}><i className="ui-facebook" /></a></li>
+                <li><a className="icon--link" href={options[0].twitter}><i className="ui-twitter" /></a></li>
+                <li><a className="icon--link" href={options[0].instagram}><i className="ui-instagram" /></a></li>
               </ul>
             </nav>
             )}
